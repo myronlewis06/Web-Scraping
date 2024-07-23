a05915eed761b423312eae518def6f45a263b057
Myron Shaun Lewis <99762727+myronlewis06@users.noreply.github.com>
2024-07-24 02:40:38 +0530

    Add files via upload

---
+++ Infinite scrolling universal code.py
@@ -0,0 +1,9 @@
+last_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(3)
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    if new_height == last_height:
+        break
+    last_height = new_height
+
